60e766754977dc755de25e9d420957ebd325f269
Mariana Sampaio <43734115+sampaiomariana@users.noreply.github.com>
2021-05-06 11:42:55 -0300

    organizando diretório para o trabalho final

--- camadaenlace.cpp
+++ camadaenlace.cpp
@@ -8,12 +8,13 @@
 
 using namespace std;
 
+/*Considerando size = 32*/
 
 void CamadaDeEnlaceTransmissoraEnquadramentoContagemDeCaracteres(int quadro[]){
 
 	int i;
 	int quantidade;
-	int size;
+	int size = 32;
 	int new_size;
 	quantidade = size/8;
 
@@ -97,6 +98,9 @@ void CamadaDeEnlaceTransmissoraEnquadramentoContagemDeCaracteres(int quadro[]){
 	}
 	
 	cout << "\n"<< endl;
+
+	//chama a próxima camada
+	CamadaEnlaceDadosReceptora(quadro);
 }
 
 void CamadaEnlaceDadosTransmissora (int quadro[]){
@@ -118,3 +122,56 @@ void CamadaEnlaceDadosTransmissoraEnquadramento(int quadro[]){
 		
 	}
 }
+/*Desenquadramento*/
+
+void CamadaEnlaceDadosReceptoraDesenquadramento(int quadro[]){
+	//implemente o código
+
+	int i;
+	int size = 32;
+	int novo_quadro[size - 8];
+	cout <<"\n Quadro Desenquadrado:";
+	cout << "\n";
+
+	for (i = 0; i < size - 8; i++){
+		novo_quadro[i] = quadro[i + 8];
+		cout << novo_quadro[i];
+	}
+
+	cout << "\n";
+
+	//chama a próxima camada
+
+	//CamadaDeAplicacaoReceptora(novo_quadro, size - 8);
+
+}
+
+void CamadaEnlaceDadosReceptoraEnquadramento(int quadro[]){
+	/*Realiza a chamada para o tipo de desenquadramento*/
+	int tipoDesenquadramento;
+	
+		cout << "\n";
+		cout << "\nTipo de Desenquadramento: \n 0 - Contagem De Caracteres \n 1 - Inserção de Bytes\n ";
+		cin >> tipoDesenquadramento;
+	
+	switch (tipoDesenquadramento){
+		case 0: // contagem de caracteres
+		CamadaEnlaceDadosReceptoraDesenquadramento(quadro);
+		break;
+		}
+}
+
+
+void CamadaEnlaceDadosReceptora(int quadro[]){
+//implemente o código
+	//chama a próxima camada
+	CamadaEnlaceDadosReceptoraEnquadramento(quadro);
+
+	//chama a próxima camada
+	//CamadaEnlaceReceptoraControleDeErro(quadro);
+
+	//chama a próxima camada
+
+	//CamadaDeAplicacaoReceptora(quadro);
+}
+
--- camadaenlace.h
+++ camadaenlace.h
@@ -9,5 +9,14 @@ void CamadaEnlaceDadosTransmissora (int quadro[]);
 
 void CamadaEnlaceDadosTransmissoraEnquadramento(int quadro[]);
 
+void CamadaEnlaceDadosReceptoraDesenquadramento(int quadro[]);
+
+void CamadaEnlaceDadosReceptora(int quadro[]);
+
+void CamadaEnlaceDadosReceptoraEnquadramento(int quadro[]);
+
+
+
+
 
 #endif //UNTITLED5_CAMADAENLACE_H
